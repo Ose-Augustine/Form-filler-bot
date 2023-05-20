91bd98f49634a3fc6ba85a76dffdd49241d29106
Ose-Augustine <osebeckley@gmail.com>
2023-05-21 00:09:17 +0100

    Interaction changes
    Wait until input field loads before interacting with it.
    Submit form by clicking button.

--- main.rb
+++ main.rb
@@ -10,15 +10,22 @@ def bot
     driver = Selenium::WebDriver.for(:edge, options: options)
 
     driver.navigate.to "https://docs.google.com/forms/d/e/1FAIpQLScRWyqpgDXKHu6pV1wEhewUIEnrHiiTnvQFgJ8SLtiGx7EebQ/viewform"
+    wait = Selenium::WebDriver::Wait.new(timeout: 100)
 
     # All questions have the same class_name
     questions = driver.find_elements(:class_name, "M7eMe")
-    input_fields = driver.find_elements(:css, ".whsOnd.zHQkBf")
-    radio_labels = driver.find_elements(:css, ".docssharedWizToggleLabeledContainer.ajBQVb")
+    input_fields = wait.until {driver.find_elements(:css, ".whsOnd.zHQkBf")}
+    radio_labels = wait.until {driver.find_elements(:css, ".docssharedWizToggleLabeledContainer.ajBQVb")}
+    submit_button = driver.find_elements(:css, ".uArJ5e.UQuaGc.Y5sE8d.VkkpIf.QvWxOd")
     title = driver.title
+
+    input_fields[0].send_keys "jamie lannister"
+    radio_labels[0].click
+    submit_button[0].click 
 
     p input_fields.length 
     p radio_labels.length 
+    p submit_button.length
 
     driver.quit
 
