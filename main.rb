39d9dd59957d9678c4f3808f0e1d740d42916276
Ose-Augustine <osebeckley@gmail.com>
2023-05-20 18:12:44 +0100

    Stop driver from opening new browser window

--- main.rb
+++ main.rb
@@ -5,7 +5,9 @@ require 'webdrivers'
 
 def bot
 
-    driver = Selenium::WebDriver.for :edge
+    options = Selenium::WebDriver::Edge::Options.new(args:['headless'])
+
+    driver = Selenium::WebDriver.for(:edge, options: options)
 
     driver.navigate.to "https://docs.google.com/forms/d/e/1FAIpQLScRWyqpgDXKHu6pV1wEhewUIEnrHiiTnvQFgJ8SLtiGx7EebQ/viewform"
 
